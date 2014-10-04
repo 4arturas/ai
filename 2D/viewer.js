ff9310d633f6f1d52c0946ec5a3ac069c7ceacba
Arturas <arturixas10@yandex.ru>
2014-10-04 07:35:38 +0300

    2D

--- 2D/viewer.js
+++ 2D/viewer.js
@@ -1,1 +1,16 @@
-hello
+function viewer_Create( width, height )
+{
+    var y, x;
+    var table, tr, td;
+    table = document.createElement('table');
+    for ( y = 0; y < height; y++ )
+    {
+        tr = document.createElement('tr');
+        table.appendChild( tr );
+        for ( x = 0; x < width; x++ )
+        {
+            td = document.createElement('td');
+            tr.appendChild( td );
+        } // end for x
+    } // end for y
+}
