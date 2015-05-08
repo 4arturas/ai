a45ace385589b8e280bb6292eb8032f88a6f1d72
Arturix <arturixas10@yandex.ru>
2015-05-08 09:54:58 +0300

    TicTacToe

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -1,71 +1,36 @@
-var BLANK = '&nbsp;';
-var HUMAN = 'X';
-var PROGRAM = 'O';
-
-var ticTacToeTable;
-var board;
-function ttt_CreateEmptyTable()
-{
-    var table = document.createElement('table');
-    var y, x;
-    var tr, td;
-    var c = 0;
-    for ( y = 0; y < 3; y++ )
-    {
-        tr = document.createElement('tr');
-        table.appendChild(tr);
-        for ( x = 0; x < 3; x++ )
-        {
-            td = document.createElement('td');
-            td.id = '' + (c++);
-            tr.appendChild( td );
-            td.innerHTML = '&nbsp;';
-        } // end for x
-    } // end for y
-    return table;
-}
-function ttt_Init()
-{
-
-    ticTacToeTable = ttt_CreateEmptyTable();
-
-    var st = document.createElement('style');
-    st.innerHTML = '#ticTacToeTable { border: 1px solid black; } #ticTacToeTable td { border: 1px solid black; padding: 5px; width:15px; height: 15px; }';
-    document.body.appendChild( st );
-    ticTacToeTable.id = 'ticTacToeTable';
-    ticTacToeTable.cellSpacing = '0';
-    ticTacToeTable.cellPadding = '0';
-    document.body.appendChild( ticTacToeTable );
-    document.body.style.padding = '40px';
-
-    board = ttt_CreateEmptyBoard();
-    ttt_EmitBoard( board );
-}
+var EMPTY = 0;
+var PROGRAM = -1;
+var HUMAN = +1;
 function ttt_EmitBoard( board )
 {
-    var i;
-    for ( i = 0; i < board.length; i++ )
+    var y, x;
+    var table = document.createElement('table');
+    table.cellSpacing = '0';
+    table.cellPadding = '0';
+    for ( y = 0; y < 3; y++ )
     {
-        document.getElementById(i + '').innerHTML = board[i];
-    }
+        var tr = document.createElement('tr');
+        table.appendChild( tr );
+        for ( x = 0; x < 3; x++ )
+        {
+            var td = document.createElement('td');
+            tr.appendChild( td );
+            var p = board[x+y*3];
+            var s = '';
+            if ( p == EMPTY )
+                s = '&nbsp;';
+            else if ( p == HUMAN )
+                s = '<span style="color:green;">X</span>';
+            else if ( p == PROGRAM )
+                s = '<span style="color:red;">O</span>';
+            td.innerHTML = s;
+        } // end for x
+    } // end for y
+    document.body.appendChild( table );
 }
-function ttt_CreateEmptyBoard()
+function ttt_WonGame( board, player )
 {
-    var b = new Array( 9 );
-    var i;
-    for ( i = 0; i < b.length; i++)
-        b[i] = BLANK;
-    return b;
-}
-
-function ttt_winCheck( board, player, x, y, z )
-{
-    return board[x] == player && board[y] == player && board[z] == player;
-}
-
-function ttt_wonCheck( board, player )
-{
-    var b = [
+    var w = [
         0,1,2,
         3,4,5,
         6,7,8,
@@ -77,131 +42,148 @@ function ttt_wonCheck( board, player )
         0,4,8,
         2,4,6
     ];
-    for ( var i = 0; i < b.length; i += 3 )
+    var i;
+    for ( i = 0; i < w.length; i += 3 )
     {
-        if ( ttt_winCheck( board, player, b[i], b[i+1], b[i+2] ) )
+        var x = w[i+0];
+        var y = w[i+1];
+        var z = w[i+2];
+        if ( w[x] == player && w[y] == player && w[z] == player )
             return true;
-    }
+    } // end for i
     return false;
 }
-function ttt_drawn_Game( board )
+function ttt_DrawnGame( board )
 {
-    for ( var i = 0; i < board.length; i++ )
-        if ( board[i] == BLANK )
+    var y;
+    for ( y = 0; y < board.length; y++ )
+        if ( board[y] == EMPTY )
             return false;
     return true;
 }
-function ttt_reachedMaxDepth( board, player )
+function ttt_ReachedMaxDepth( board )
 {
-    return ttt_wonCheck( board, HUMAN ) ||  ttt_wonCheck( board, PROGRAM ) || ttt_drawn_Game( board );
+    return ttt_WonGame( board, HUMAN ) || ttt_WonGame( board, PROGRAM ) || ttt_DrawnGame( board );
 }
-function ttt_evaluatePosition( board, player )
+function ttt_PossibleMoves( board, player )
 {
-    var i, count = 0;
+    var i, j;
+    var move = [];
     for ( i = 0; i < board.length; i++ )
-        if ( board[i] == BLANK )
-            count++;
+    {
+        if ( board[i] != EMPTY )
+            continue;
+        var b = new Array( board.length );
+        for ( j = 0; j < board.length; j++ )
+            b[j] = board[j];
+        b[i] = player;
+        move.push( b );
+    } // end for i
+    return move;
+}
+function ttt_EvaluatePosition( board, player )
+{
+    var i, count = 0.0;
+    for ( i = 0; i < board.length; i++ )
+        if ( board[i] == EMPTY )
+            count += 1.0;
+    count = 10.0 - count;
 
-    count = 10 - count;
     var blank = 1.0;
     if ( board[4] == HUMAN && player == HUMAN )
         blank += 0.4;
     if ( board[4] == PROGRAM && player == PROGRAM )
         blank -= 0.4;
 
-    if ( ttt_wonCheck( board, HUMAN ) )
+    if ( ttt_WonGame( board, HUMAN ) )
         return blank + ( 1.0 / count );
-    if ( ttt_wonCheck( board, PROGRAM ) )
-        return -( blank + ( 1.0 / count ) );
+    if ( ttt_WonGame( board, PROGRAM ) )
+        return -(blank + ( 1.0 / count ) );
+
     return blank - 1.0;
 }
-function ttt_possibleBoards( board, player )
+function ttt_AlphaBeta( board, player, depth, alpha, beta )
 {
-    var count = 0, i;
-    for ( i = 0; i < board.length; i++ )
-        if ( board[i] == BLANK )
-            count++;
-
-    if ( count == 0 )
-        return null;
-
-    var boards = new Array( count );
-    count = 0;
-    for ( i = 0; i < board.length; i++ )
+    var v, value;
+    if ( ttt_ReachedMaxDepth( board ) )
     {
-        if ( board[i] != BLANK )
-            continue;
-
-        var b = new Array( board.length );
-        for ( var j = 0; j < board.length; j++ )
-            b[j] = board[j];
-        b[i] = player;
-        boards[count++] = b;
-    } // end for i
-    return boards;
-}
-function ttt_alphaBetaPrunning( depth, board, player, alpha, beta )
-{
-    var v;
-    if ( ttt_reachedMaxDepth( board, player ) )
-    {
-        v = new Array(2);
-        v[0] = ttt_evaluatePosition( board, player );
-        v[1] = null;
-        return v;
+        value = ttt_EvaluatePosition( board, player );
+        return [ value, null ];
     }
 
-    var i;
-    var best = new Array();
-    var boards = ttt_possibleBoards( board, player );
-    for ( i = 0; i < boards.length; i++ )
+    var best = [];
+    var move = ttt_PossibleMoves( board, player );
+    for ( var i = 0; i < move.length; i++ )
     {
-        v = ttt_alphaBetaPrunning( depth+1, boards[i], (player==HUMAN)?PROGRAM:HUMAN, -alpha, -beta );
-        var value = v[0];
+        v = ttt_AlphaBeta( move[i], player==HUMAN?PROGRAM:HUMAN, depth+1, -beta, -alpha );
+        if ( player == HUMAN )
+            value = v[0];
+        else
+            value = -v[0];
         if ( value > beta )
         {
             beta = value;
-            best = new Array();
-            best.push( boards[i] );
+            best = [];
+            best.push( move[i] );
             for ( var j = 1; j < v.length; j++ )
-                best.push( v[j] );
-        } // end if
+            {
+                var o = v[j];
+                if ( o == null )
+                    continue;
+                best.push( o );
+            }
+        }
         if ( beta >= alpha )
             break;
     } // end for i
-
-    v = new Array();
+    v = [];
     v.push( beta );
     for ( i = 0; i < best.length; i++ )
         v.push( best[i] );
     return v;
 }
-function ttt_alphaBeta( depth, player, board )
+function ttt_PlayGame( board, player )
 {
-    return ttt_alphaBetaPrunning( depth, board, player, 1000000, -1000000 );
-}
-function ttt_playGame()
-{
-    var player = HUMAN;
-    while ( 1 == 1 )
+    ttt_EmitBoard( board );
+
+    if ( ttt_WonGame( board, player ) )
     {
-        if ( ttt_wonCheck( board, HUMAN ) )
+        console.log( (player==HUMAN?'Human won':'Program won') );
+        return;
+    }
+    if ( ttt_DrawnGame( board ) )
+    {
+        console.log( 'Drawn game' );
+        return;
+    }
+
+
+    setTimeout( function() {
+        var p = player==HUMAN?PROGRAM:HUMAN;
+        var alpha, beta;
+        var v = 1000000;
+        if ( p == HUMAN )
         {
-            alert( 'Human won' );
-            break;
+            alpha = v;
+            beta = -v;
         }
-        if ( ttt_wonCheck( board, PROGRAM ) )
+        else
         {
-            alert( 'Program won' );
-            break;
+            alpha = v;
+            beta = -v;
         }
-        if ( ttt_drawn_Game( board ) )
-        {
-            alert( 'Drawn game' );
-            break;
-        }
-        board = ttt_alphaBeta( 0, player, board );
-        ttt_EmitBoard( board );
-        player = (player==HUMAN) ? PROGRAM : HUMAN;
-    } // end while
+        var vv = ttt_AlphaBeta( board, p, 0, alpha, beta );
+        v = vv[0];
+        console.log( v );
+        ttt_PlayGame( vv[1], p );
+    }, 500 );
 }
+function ttt_CreateEmptyBoard()
+{
+    var b = [];
+    var y;
+    for ( y = 0; y < 9; y++ )
+        b.push( EMPTY );
+    return b;
+}
+ttt_PlayGame( ttt_CreateEmptyBoard(), PROGRAM );
