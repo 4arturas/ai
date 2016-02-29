9854d123b0802b03ff2ee7e04275a83ba10e3506
Arturix <arturixas10@yandex.ru>
2016-02-29 11:09:28 +0200

    astar3

--- astar/astar3.js
+++ astar/astar3.js
@@ -2,9 +2,9 @@ function pq_Create( size )
 {
     return {
         count: 0,
-        arr: new Array( size+1 ),
-        fnHeuristics: function( n0, n1 ) { console.log('Not implemented fnHeuristics'); },
-        fnEquals: function( n0, n1 ) { console.log('Not implemented fnEquals'); }
+        arr: new Array(size+1),
+        fnHeur: function(n0,n1) { console.log('Not implemented fnHeur'); },
+        fnEquals: function( n0, n1 ) { console.log('Not implemented fnEquals') }
     };
 }
 function pq_Enqueue( pq, n )
@@ -15,12 +15,12 @@ function pq_Enqueue( pq, n )
     child = pq.count;
     while ( parent > 0 )
     {
-        if ( pq.fnHeuristics( pq.arr[parent], n ) )
+        if ( pq.fnHeur( pq.arr[parent], n ) )
         {
             pq.arr[child] = pq.arr[parent];
             child = parent;
             parent = Math.floor( parent/2 );
-        }
+        } // end if
         else
             break;
     } // end while
@@ -36,14 +36,13 @@ function pq_Dequeue( pq, idx )
     while ( pq.count > child )
     {
         if ( pq.count-1 > child )
-            if ( pq.fnHeuristics( pq.arr[child], pq.arr[child+1] ) )
+            if ( pq.fnHeur( pq.arr[child], pq.arr[child+1] ) )
                 child++;
-
-        if ( pq.fnHeuristics( n, pq.arr[child] ) )
+        if ( pq.fnHeur( n, pq.arr[child] ) )
         {
             pq.arr[parent] = pq.arr[child];
             parent = child;
-            child = child*2;
+            child *= 2;
         }
         else
             break;
@@ -53,19 +52,18 @@ function pq_Dequeue( pq, idx )
 }
 function pq_GetIdx( pq, n )
 {
-    var i;
-    for ( i = 1; i <= pq.count; i++ )
-        if ( pq.fnEquals( pq.arr[i], n ) )
+    for ( var i = 1; i <= pq.count; i++ )
+        if ( pq.fnEquals(pq.arr[i], n ) )
             return i;
     return -1;
 }
 function pq_Test()
 {
     var i;
-    var n;
     var size = 10;
+    var n;
     var pq = pq_Create( size );
-    pq.fnHeuristics = function( n0, n1 )
+    pq.fnHeur = function( n0, n1 )
     {
         return n0.f > n1.f;
     };
@@ -73,13 +71,14 @@ function pq_Test()
     {
         return n0.f == n1.f;
     };
+
     for ( i = 0; i < size; i++ )
     {
         n = { f: i };
         pq_Enqueue( pq, n );
-    }
+    } // end for n
 
-    n = { f: 1 };
+    n = {f:0};
     i = pq_GetIdx( pq, n );
     pq_Dequeue( pq, i );
     while ( pq.count > 0 )
@@ -94,12 +93,8 @@ pq_Test();
 function rnd_Int( x, y )
 {
     return Math.floor(
-        Math.random() * ( y - x + 1 ) + x
+      Math.random() * ( y - x + 1 ) + x
     );
-}
-function _log( l )
-{
-    console.log( l );
 }
 
 var AS_HEIGHT = 20;
@@ -107,118 +102,116 @@ var AS_WIDTH = 20;
 
 function as_CreateTable()
 {
+    var asTable = document.getElementById('asTable');
     var y, x;
     var tr, td;
-    var asTable = document.getElementById('asTable');
     for ( y = 0; y < AS_HEIGHT; y++ )
     {
         tr = document.createElement('tr');
-        asTable.appendChild(tr);
+        asTable.appendChild( tr );
         for ( x = 0; x < AS_WIDTH; x++ )
         {
             td = document.createElement('td');
-            tr.appendChild(td);
-            td.setAttribute('id', y + '_' + x);
+            tr.appendChild( td );
+            td.setAttribute('id', y+'_'+x);
         } // end for x
     } // end for y
 }
 as_CreateTable();
 
-function as_NodesEqual( n0, n1 )
-{
-    return n0.x == n1.x && n0.y == n1.y;
-}
 function as_CreateNode( y, x )
 {
     return {
         y: y, x: x,
-        h: 0, g: 0, f: 0,
-        passable: 1, closed: 0, visited: 0, cost: 0
+        g: 0, h:0, f: 0,
+        closed: 0, visited: 0, passable: 1, cost: 0,
+        parrent: null
     };
 }
-function as_GetNodeTD( y, x )
+function as_CreateMap()
 {
-    return document.getElementById( y + "_" + x );
-}
-function as_Create()
-{
-    var y, x;
-    var n;
-    var start = as_CreateNode( 0, 0 );
-    var goal = as_CreateNode( AS_HEIGHT-1, AS_WIDTH-1 );
     var asMap = {
-        start: start,
-        goal: goal,
+        start: null,
+        goal: null,
         map: null
     };
     asMap.map = new Array( AS_HEIGHT );
-    for ( y = 0; y < asMap.map.length; y++ )
+    var y, x;
+    for ( y = 0; y < AS_HEIGHT; y++ )
     {
         asMap.map[y] = new Array( AS_WIDTH );
-        for ( x = 0; x < asMap.map[y].length; x++ )
+        for ( x = 0; x < AS_WIDTH; x++ )
         {
-            n = as_CreateNode( y, x );
-            n.passable = rnd_Int( 0, 5 ) == 5 ? 0 : 1;
+            var n = as_CreateNode( y, x );
+            n.passable = rnd_Int( 0, 4 ) == 0 ? 0 : 1;
             asMap.map[y][x] = n;
         } // end for x
     } // end for y
-    asMap.start.passable = 1;
-    asMap.goal.passable = 1;
+    asMap.start = asMap.map[0][0];
+    asMap.goal = asMap.map[AS_HEIGHT-1][AS_WIDTH-1];
+
+    asMap.map[0][0].passable = 1;
+    asMap.map[AS_HEIGHT-1][AS_WIDTH-1].passable = 1;
 
     return asMap;
 }
-function as_EmitTable( asMap )
+function as_GetTD( y, x )
 {
-    var y, x;
-    var n;
+    return document.getElementById(y+'_'+x);
+}
+function as_MapEmit( asMap )
+{
     var td;
+    var y, x;
     for ( y = 0; y < asMap.map.length; y++ )
     {
         for ( x = 0; x < asMap.map[y].length; x++ )
         {
-            td = as_GetNodeTD( y, x );
-            n = asMap.map[y][x];
+            var n = asMap.map[y][x];
 
-            if (n.passable==1)
-                td.style.backgroundColor = 'white';
-            else
+            td = as_GetTD( y, x );
+            if (n.passable==0)
                 td.style.backgroundColor = 'black';
+            else
+                td.style.backgroundColor = 'white';
+            if (n.visited == 1)
+                td.style.backgroundColor = 'gray';
 
-            if ( n.visited == 1)
-            {
-                console.log( n.visited );
-                td.style.backgroundColor = 'lightblue';
-            }
-
+            //n.cost = rnd_Int( 0, AS_WIDTH+AS_HEIGHT );
         } // end for x
     } // end for y
-    td = as_GetNodeTD( 0, 0 );
-    td.style.backgroundColor = 'yellow';
-    td = as_GetNodeTD( AS_HEIGHT-1, AS_WIDTH-1 );
+    td = as_GetTD( asMap.goal.y, asMap.goal.x );
     td.style.backgroundColor = 'red';
+
+    td = as_GetTD( asMap.start.y, asMap.start.x );
+    td.style.backgroundColor = 'yellow';
 }
 function as_Manhattan( n0, n1 )
 {
     var y = Math.abs( n0.y - n1.y );
     var x = Math.abs( n0.x - n1.x );
-    return y*y+x*x;
+    return (y*y)+(x*x);
+}
+function as_NodesEqual( n0, n1 )
+{
+    return n0.y == n1.y && n0.x == n1.x;
 }
 function as_Main()
 {
-    var dirY = [-1,+1,-0,+0];
-    var dirX = [-0,+0,-1,+1];
-    var asMap = as_Create();
-    as_EmitTable( asMap );
-
+    var dirY = [ -1, +1, -0, +0 ];
+    var dirX = [ -0, +0, -1, +1 ];
+    var asMap = as_CreateMap();
+    as_MapEmit( asMap );
     var pq = pq_Create( AS_HEIGHT*AS_WIDTH );
-    pq.fnHeuristics = function( n0, n1 )
+    pq.fnHeur = function( n0, n1 )
     {
-        return n0.f > n1.f;
+        return n0.f > n1.f ;
     };
     pq.fnEquals = function( n0, n1 )
     {
         return as_NodesEqual( n0, n1 );
     };
+
     pq_Enqueue( pq, asMap.start );
     while ( pq.count > 0 )
     {
@@ -226,8 +219,16 @@ function as_Main()
         pq_Dequeue( pq, 1 );
         if ( as_NodesEqual( currNode, asMap.goal ) )
         {
-            as_EmitTable( asMap );
-            _log( 'found' );
+            as_MapEmit( asMap );
+            console.log('found');
+            var nn = currNode;
+            while ( nn != null )
+            {
+                var td = as_GetTD( nn.y, nn.x );
+                td.style.backgroundColor = 'lightblue';
+                nn = nn.parrent;
+            }
+
             return;
         }
         currNode.closed = 1;
@@ -239,16 +240,16 @@ function as_Main()
             if ( 0 > x || x >= AS_WIDTH ) continue;
 
             var n = asMap.map[y][x];
-            if ( n.closed == 1 ) continue;
-            if ( n.passable == 0 ) continue;
-
+            if (n.closed == 1) continue;
+            if (n.passable == 0 ) continue;
             var g = n.cost + currNode.g;
-            if (n.visited == 0 || n.g > g )
+            if (n.visited==0 || n.g > g )
             {
                 n.g = g;
                 n.h = as_Manhattan( n, asMap.goal );
                 n.f = n.g + n.h;
-                if ( n.visited == 1 )
+                n.parrent = currNode;
+                if (n.visited == 1 )
                 {
                     var idx = pq_GetIdx( pq, n );
                     pq_Dequeue( pq, idx );
@@ -258,6 +259,7 @@ function as_Main()
             } // end if
         } // end for i
     } // end while
-    as_EmitTable( asMap );
+    as_MapEmit( asMap );
+    console.log('not found');
 }
 as_Main();
