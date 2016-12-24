6972d5d7e2a5c7dfa1e3c82d26a170cca0cdd959
Arturix <arturixas10@yandex.ru>
2016-12-24 16:16:50 +0200

    probability

---
+++ probability/probability1.js
@@ -0,0 +1,20 @@
+function perm( nAttempts )
+{
+    var res = nAttempts;
+    for ( var n = nAttempts-1; n >= 2; n-- )
+    {
+        res *= n;
+    }
+    if ( res == 0 ) res = 1; // Jeigu rezultatas = 0 reiskia 1
+    return res;
+}
+function N_choose_K( nAttempts, kScores )
+{
+    var i;
+    var nomitator = perm( nAttempts );
+    var denominator = perm( kScores * (nAttempts-kScores));
+    return nomitator/denominator;
+
+}
+var r = N_choose_K( 5, 5 );
+console.log( r );
