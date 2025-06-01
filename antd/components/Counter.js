49398b289304d88132dfad096b7dee77cc47707b
Your Name <you@example.com>
2025-06-01 17:26:59 +0300

    2025.06.01

--- antd/components/Counter.js
+++ antd/components/Counter.js
@@ -2,15 +2,13 @@ const { Button } = antd;
 
 function Counter() {
     const [count, setCount] = React.useState(0);
-    const { appContext, setAppContext } = React.useContext(AppContext);
 
     const handleClick = () => {
         setCount(count + 1);
-        setAppContext( { ...appContext, activeTabKey: "1" } );
     };
     return (
         <div>
-            <Button type="primary" onClick={handleClick} config={{ prefixCls: 'no-wave' }}>
+            <Button type="primary" onClick={handleClick}>
                 Click me!
             </Button>
             <p>Button clicked {count} times</p>
